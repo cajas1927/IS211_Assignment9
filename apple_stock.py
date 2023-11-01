0f5a15426141be3b5d12dc8252a12dd147ab3e79
cajas1927 <123519445+cajas1927@users.noreply.github.com>
2023-11-01 08:33:37 +0530

    Update apple_stock.py

--- apple_stock.py
+++ apple_stock.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Define the URL for CBS Football Stats
-url = "https://www.cbssports.com/nfl/stats/"
+# Define the URL for Apple stock data on Yahoo Finance
+url = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
 
 # Send an HTTP GET request to the URL
 response = requests.get(url)
@@ -12,31 +12,15 @@ if response.status_code == 200:
     # Parse the HTML content of the page using BeautifulSoup
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find the dropdown menu
-    dropdown = soup.find('select', {'id': 'seasonsDropdown'})
+    # Find the table containing historical stock prices
+    table = soup.find('table', {'data-test': 'historical-prices'})
 
-    # Check if the dropdown element is found
-    if dropdown:
-        # Select the "regular" option if it exists
-        regular_option = dropdown.find('option', {'value': '2'})
-        if regular_option:
-            regular_option['selected'] = 'selected'
-
-            # Find the table containing the top 20 touchdown leaders
-            touchdown_table = soup.find('table', {'id': 'player-stats'})
-
-            # Extract and print the data for the top 20 touchdown leaders
-            rows = touchdown_table.find_all('tr')[1:21]  # Skip the header row
-            for row in rows:
-                columns = row.find_all('td')
-                player = columns[0].text.strip()
-                position = columns[1].text.strip()
-                team = columns[2].text.strip()
-                touchdowns = columns[3].text.strip()
-                print(f"Player: {player}, Position: {position}, Team: {team}, Touchdowns: {touchdowns}")
-        else:
-            print("Regular option not found in the dropdown.")
-    else:
-        print("Dropdown menu not found on the page.")
+    # Extract and print the date and close price for all dates
+    rows = table.find_all('tr')[1:]  # Skip the header row
+    for row in rows:
+        columns = row.find_all('td')
+        date = columns[0].text.strip()
+        close_price = columns[4].text.strip()
+        print(f"Date: {date}, Close Price: {close_price}")
 else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
